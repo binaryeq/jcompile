b63b1650981e29a850747f4cdf482e990654634d
Tim White <walton.timothy.james.white@gmail.com>
2023-10-31 15:28:10 +1300

    No output to stdout; exit code = 0 iff feature detected

--- detect-bytecode-features.sh
+++ detect-bytecode-features.sh
@@ -2,10 +2,10 @@
 
 case "$1" in
 	JEP181)
-		javap -c -v "$2" | grep -E 'static .* access\$[0-9]+\('
+		javap -c -v "$2" | grep -E -A 2 'static .* access\$[0-9]+\(' | grep -q -E -w 'ACC_SYNTHETIC'
 		;;
 
 	JEP280)
-		javap -c -v "$2" | grep -E '// InvokeDynamic #0:makeConcatWithConstants:\(Ljava/lang/String;Ljava/lang/String;\)Ljava/lang/String;$'
+		javap -c -v "$2" | grep -q -E '// InvokeDynamic #0:makeConcatWithConstants:\(Ljava/lang/String;Ljava/lang/String;\)Ljava/lang/String;$'
 		;;
 esac
