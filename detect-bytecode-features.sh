26e4fe6a4b4dd47bcb5c2c78f647030432effddf
Tim White <walton.timothy.james.white@gmail.com>
2023-10-31 17:46:13 +1300

    Additionally catch JEP181 in very old (JDK1.2) ecj builds

--- detect-bytecode-features.sh
+++ detect-bytecode-features.sh
@@ -4,7 +4,7 @@ case "$1" in
 	JEP181)
 		# JDK >= 11 uses "nests" to allow inner classes to access private members of outer classes directly, instead of creating synthetic methods in the outer class: https://openjdk.org/jeps/181
 		# A positive detection (exit code 0) means the *old* (JDK < 11) behaviour, since this is easier to test for.
-		javap -c -v "$2" | grep -E -A 2 'static .* access\$[0-9]+\(' | grep -q -E -w 'ACC_SYNTHETIC'
+		javap -c -v "$2" | grep -E -A 3 'static .* access\$[0-9]+\(' | grep -q -E -w 'ACC_SYNTHETIC|Synthetic: true'
 		;;
 
 	JEP280)
