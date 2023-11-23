a86e8021ebf506bde5d0cbb79aa504f378b7307c
Tim White <walton.timothy.james.white@gmail.com>
2023-11-23 15:58:42 +1300

    Generalise OpenJDKVersionsComparator to handle ecj compiler lineage without choking on semver-like versions

--- oracle-construction/src/main/java/nz/ac/wgtn/shadedetector/jcompile/oracles/comparators/OpenJDKVersionsComparator.java
+++ oracle-construction/src/main/java/nz/ac/wgtn/shadedetector/jcompile/oracles/comparators/OpenJDKVersionsComparator.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 
 import java.util.Comparator;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static nz.ac.wgtn.shadedetector.jcompile.oracles.comparators.SemVer.compareSemVer;
@@ -11,16 +12,24 @@ import static nz.ac.wgtn.shadedetector.jcompile.oracles.comparators.SemVer.parse
 
 public class OpenJDKVersionsComparator implements Comparator<String> {
 
-    public static final String OPENJDK_SEMVER = "openjdk-\\d+\\.\\d+\\.\\d+";
-    public static final Predicate<String> isOpenJdkSemVer = Pattern.compile(OPENJDK_SEMVER).asPredicate();
+    public static final String OPENJDK_SEMVER = "^(openjdk|ecj)-(\\d+\\.\\d+\\.\\d+)";
+    public static final Pattern OPENJDK_SEMVER_REGEX = Pattern.compile(OPENJDK_SEMVER);
 
     @Override
     public int compare(String o1, String o2) {
-        Preconditions.checkArgument(isOpenJdkSemVer.test(o1));
-        Preconditions.checkArgument(isOpenJdkSemVer.test(o2));
+        Matcher m1 = OPENJDK_SEMVER_REGEX.matcher(o1);
+        Matcher m2 = OPENJDK_SEMVER_REGEX.matcher(o2);
+        if (m1.find() && m2.find()) {
+            int lineageDiff = m1.group(0).compareTo(m2.group(0));
+            if (lineageDiff != 0) {
+                return lineageDiff;      // Compiler lineages differ
+            }
+        } else {
+            throw new IllegalArgumentException("One of the operands did not match " + OPENJDK_SEMVER);
+        }
 
-        String version1 = o1.substring(o1.lastIndexOf("-")+1);
-        String version2 = o2.substring(o2.lastIndexOf("-")+1);
+        String version1 = m1.group(1);
+        String version2 = m2.group(1);
 
         int[] semver1 = parseSemVer(version1);
         int[] semver2 = parseSemVer(version2);
