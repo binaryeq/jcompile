89aea0e0ea888ab0a9432e833200a2f6630731e2
Tim White <walton.timothy.james.white@gmail.com>
2023-11-28 18:39:56 +1300

    Always detect all features

--- detect-bytecode-features.pl
+++ detect-bytecode-features.pl
@@ -3,8 +3,6 @@
 use strict;
 use warnings;
 
-my $feature = shift;
-
 my $currentClass;
 my %has;
 my $inFunction = 0;
@@ -23,23 +21,19 @@ while (<JAVAP>) {
 		%has = ();
 		$inFunction = 0;
 	} else {
-		if ($feature eq 'JEP181') {
-			# JDK >= 11 uses "nests" to allow inner classes to access private members of outer classes directly, instead of creating synthetic methods in the outer class: https://openjdk.org/jeps/181
-			# A positive detection (exit code 0) means the *old* (JDK < 11) behaviour, since this is easier to test for.
-			if (/^  ([^ ].*);$/ && $1 =~ /static .* access\$[0-9]+\(/) {
-				$inFunction = 1;
-			} elsif (/^}?\s*$/) {
-				$inFunction = 0;
-			}
-
-			$has{JEP181} = 1 if $inFunction && /ACC_SYNTHETIC|Synthetic: true/;
-		} elsif ($feature eq 'JEP280') {
-			# JDK >= 9 uses special invokedynamic calls to concatenate string literals instead of StringBuilder: https://openjdk.org/jeps/280, https://docs.oracle.com/javase/9/docs/api/java/lang/invoke/StringConcatFactory.html
-			# A positive detection (exit code 0) means the *new* (JDK >= 9) behaviour, since this is easier to test for.
-			$has{JEP280} = 1 if m!^ *#[0-9]+ = Class +#[0-9]+ +// java/lang/invoke/StringConcatFactory$!;
-		} else {
-			die "Unrecognised feature '$feature', aborting.";
+		# JDK >= 11 uses "nests" to allow inner classes to access private members of outer classes directly, instead of creating synthetic methods in the outer class: https://openjdk.org/jeps/181
+		# A positive detection (exit code 0) means the *old* (JDK < 11) behaviour, since this is easier to test for.
+		if (/^  ([^ ].*);$/ && $1 =~ /static .* access\$[0-9]+\(/) {
+			$inFunction = 1;
+		} elsif (/^}?\s*$/) {
+			$inFunction = 0;
 		}
+
+		$has{JEP181} = 1 if $inFunction && /ACC_SYNTHETIC|Synthetic: true/;
+
+		# JDK >= 9 uses special invokedynamic calls to concatenate string literals instead of StringBuilder: https://openjdk.org/jeps/280, https://docs.oracle.com/javase/9/docs/api/java/lang/invoke/StringConcatFactory.html
+		# A positive detection (exit code 0) means the *new* (JDK >= 9) behaviour, since this is easier to test for.
+		$has{JEP280} = 1 if m!^ *#[0-9]+ = Class +#[0-9]+ +// java/lang/invoke/StringConcatFactory$!;
 	}
 }
 
