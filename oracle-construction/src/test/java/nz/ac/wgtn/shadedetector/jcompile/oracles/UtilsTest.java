4650b734711b0aa474c82223a0a573f832f53936
Tim White <walton.timothy.james.white@gmail.com>
2023-11-29 13:05:06 +1300

    Add passing tests for getTopLevelClass()

--- oracle-construction/src/test/java/nz/ac/wgtn/shadedetector/jcompile/oracles/UtilsTest.java
+++ oracle-construction/src/test/java/nz/ac/wgtn/shadedetector/jcompile/oracles/UtilsTest.java
@@ -2,9 +2,14 @@ package nz.ac.wgtn.shadedetector.jcompile.oracles;
 
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
 import java.nio.file.Path;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static nz.ac.wgtn.shadedetector.jcompile.oracles.TestUtils.JARS;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UtilsTest {
 
@@ -33,5 +38,40 @@ public class UtilsTest {
     public void testPackageInfoDetector2() {
         Path p = Path.of("/org/apache/commons/csv/IOUtils.class");
         assertFalse(Utils.isPackageInfo(p));
+    }
+
+    @Test
+    public void testGetTopLevelClass_alreadyTopLevel() {
+        Path p = Path.of("/abc/SomeClass.class");
+        assertEquals(p, Utils.getTopLevelClass(p));
+    }
+
+    @Test
+    public void testGetTopLevelClass_innerClass() {
+        Path p = Path.of("/abc/SomeClass$InnerClass.class");
+        assertEquals(Path.of("/abc/SomeClass.class"), Utils.getTopLevelClass(p));
+    }
+
+    @Test
+    public void testGetTopLevelClass_anonymousInnerClass() {
+        Path p = Path.of("/abc/SomeClass$123.class");
+        assertEquals(Path.of("/abc/SomeClass.class"), Utils.getTopLevelClass(p));
+    }
+
+    @Test
+    public void testGetTopLevelClass_multiLevelInnerClass() {
+        Path p = Path.of("/abc/SomeClass$TopInnerClass$42$LowestInner.class");
+        assertEquals(Path.of("/abc/SomeClass.class"), Utils.getTopLevelClass(p));
+    }
+
+    @Test
+    public void testGetTopLevelClass_preservesFileSystem() throws URISyntaxException, IOException {
+        try (FileSystem zipfs = Utils.getJarFileSystem(JARS.resolve("openjdk-9.0.1").resolve("commons-codec-1.11.jar"))) {
+            Path p = zipfs.getPath("/abc/SomeClass.class");
+            Path defaultFileSystemPath = Path.of("/abc/SomeClass.class");
+            Path result = Utils.getTopLevelClass(p);
+            assertEquals(result.getFileSystem(), p.getFileSystem());
+            assertNotEquals(result, defaultFileSystemPath);
+        }
     }
 }
