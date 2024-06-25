5a7ff5af69ee8a92c7a67ae3d77150c08ef9a664
Tim White <walton.timothy.james.white@gmail.com>
2024-06-25 22:21:27 +1200

    Always generate rules for BREAKING and POTENTIALLY_BREAKING and nothing else

--- make-revapi-runner.sh
+++ make-revapi-runner.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-# Input on stdin should consist of the logging output of AdjacentVersionSameArtifactAndCompilerClassOracle (lines of the form "analysing XYZ.jar vs ABC.jar", or "XYZ.jar\tABC.jar").
+# Input on stdin should consist of the logging output of AdjacentVersionSameArtifactAndCompilerJarOracle (lines of the form "analysing XYZ.jar vs ABC.jar", or "XYZ.jar\tABC.jar").
 # Outputs a Makefile to stdout that will run revapi on each mentioned pair of jars and store the JSON output in a file that mentions both jars' versions.
-# Optionally supply a minimum severity (EQUIVALENT, NON_BREAKING, POTENTIALLY_BREAKING or BREAKING (the default)) on the command line.
+# Of the possible minimum severities (EQUIVALENT, NON_BREAKING, POTENTIALLY_BREAKING or BREAKING (the default)), we only care about BREAKING and POTENTIALLY_BREAKING.
 
-MINSEVERITY="${1:-BREAKING}"
 REVAPI_BASE_CMD="revapi.sh --extensions=org.revapi:revapi-java:0.28.1,org.revapi:revapi-reporter-json:0.5.0 -Drevapi.reporter.json.minSeverity"		# Must be followed by, e.g., "=BREAKING"
 
-echo "all:"		# Make defaults to first goal
+echo "all: all.BREAKING all.POTENTIALLY_BREAKING"		# Make defaults to first goal
 echo
-echo "MINSEVERITY := $MINSEVERITY"		# So the value passed to this script becomes the default, but it can be overridden at make time
 echo "JCOMPILE_ROOT := $(git rev-parse --show-toplevel)"
 echo
 echo "%.tsv: %.json"
 /bin/echo -e "\\t\$(JCOMPILE_ROOT)/summarise-revapi-json-to-tsv.sh < \$< > \$@"
 echo
-perl -lne 'if (m%^(?:analysing: )?(\S+)\.jar(?: vs |\t)(\S+/([^/]+))\.jar%) { my $bn = "$1__vs__$3.revapi.\$(MINSEVERITY)"; print "$bn.json:\n\t'"$REVAPI_BASE_CMD"'=\$(MINSEVERITY) --old=$1.jar --new=$2.jar -Drevapi.reporter.json.output=\$\@\nall: $bn.tsv\n"; }'
+perl -lne 'sub f($) { my ($MINSEVERITY) = @_; if (m%^(?:analysing: )?(\S+)\.jar(?: vs |\t)(\S+/([^/]+))\.jar%) { my $bn = "$1__vs__$3.revapi.$MINSEVERITY"; print "$bn.json:\n\t'"$REVAPI_BASE_CMD"'=$MINSEVERITY --old=$1.jar --new=$2.jar -Drevapi.reporter.json.output=\$\@\nall.$MINSEVERITY: $bn.tsv\n"; } } f("BREAKING"); f("POTENTIALLY_BREAKING");'
